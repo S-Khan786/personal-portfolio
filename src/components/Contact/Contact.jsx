d65238fc6e444e7a351e74df08eb5d1686718ac0
S-Khan786 <osahilkhan654@gmail.com>
2025-01-30 10:04:31 +0530

    Added projects and contact section

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+import styles from "./Contact.module.css";
+
+const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to react out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src="../../../assets/contact/emailIcon.png" alt="Email Icon" />
+          <a href="mailto:myemail@email.com">myemail@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src="../../../assets/contact/linkedinIcon.png"
+            alt="Linkedin Icon"
+          />
+          <a href="https://www.linkedin.com/in/mohammad-sahil-a23696200">
+            Linkedin.com/myname
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src="../../../assets/contact/githubIcon.png" alt="Github Icon" />
+          <a href="https://github.com/S-Khan786">github.com/myname</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Contact;
